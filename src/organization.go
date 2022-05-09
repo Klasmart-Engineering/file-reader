f985e39c97998f671ade3e9ea28165e5bd520d50
An Nguyen <annguyen@KLUKMAC412536.local>
2022-05-09 22:33:58 +0100

    Refactor Operation

--- src/organization.go
+++ src/organization.go
@@ -2,21 +2,31 @@ package src
 
 import (
 	avro "file_reader/avro_gencode"
+	orgPb "file_reader/src/protos/onboarding"
 	"os"
 
 	"github.com/google/uuid"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 const (
-	organizationTopic = "organization"
-	orgSchemaFilename = "organization.avsc"
+	organizationTopic      = "organization"
+	organizationProtoTopic = "organization_proto"
+	orgSchemaFilename      = "organization.avsc"
+	orgProtoSchemaFileName = "onboarding.proto"
 )
 
 var Organization = Operation{
-	topic:         organizationTopic,
-	key:           "",
-	schemaIDBytes: schemaRegistryClient.getSchemaIdBytes(orgSchemaFilename, organizationTopic),
-	rowToSchema:   rowToOrganization,
+	topic:       organizationTopic,
+	key:         "",
+	rowToSchema: rowToOrganization,
+}
+
+var OrganizationProto = Operation{
+	topic:            organizationProtoTopic,
+	key:              "",
+	schema:           schemaRegistryClient.getProtoSchema(orgProtoSchemaFileName, organizationProtoTopic),
+	rowToProtoSchema: rowToOrganizationProto,
 }
 
 // ToDo: add logic for stripping header and figuring out column order
@@ -31,4 +41,17 @@ func rowToOrganization(row []string) avroCodec {
 		Organization_name: row[1],
 	}
 	return avro.Organization{Payload: pl, Metadata: md}
+}
+
+func rowToOrganizationProto(row []string) *orgPb.Organization {
+	md := orgPb.Metadata{
+		OriginApplication: wrapperspb.String(os.Getenv("METADATA_ORIGIN_APPLICATION")),
+		Region:            wrapperspb.String(os.Getenv("METADATA_REGION")),
+		TrackingId:        wrapperspb.String(uuid.NewString()),
+	}
+	pl := orgPb.OrganizationPayload{
+		Uuid: wrapperspb.String(row[0]),
+		Name: wrapperspb.String(row[1]),
+	}
+	return &orgPb.Organization{Payload: &pl, Metadata: &md}
 }
