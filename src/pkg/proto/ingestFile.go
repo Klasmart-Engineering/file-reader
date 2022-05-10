a02b58146872b78edf4a53dd3daa89964ece4e03
An Nguyen <annguyen@KLUKMAC412536.local>
2022-05-10 10:27:17 +0100

    Create a separate package for proto logic

--- src/pkg/proto/ingestFile.go
+++ src/pkg/proto/ingestFile.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/csv"
 	"file_reader/src/instrument"
+<<<<<<< HEAD
 	"file_reader/src/log"
 	orgPb "file_reader/src/protos/onboarding"
 	"fmt"
@@ -16,19 +17,42 @@ import (
 	"github.com/riferrei/srclient"
 	"github.com/segmentio/kafka-go"
 	"github.com/segmentio/kafka-go/compress"
+=======
+	orgPb "file_reader/src/protos/onboarding"
+	"io"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/riferrei/srclient"
+	"github.com/segmentio/kafka-go"
+	"github.com/segmentio/kafka-go/compress"
+
+	"google.golang.org/protobuf/proto"
+>>>>>>> 989d433 (Create a separate package for proto logic)
 )
 
 type Config struct {
 	BrokerAddrs []string
 	Reader      io.Reader
 	Context     context.Context
+<<<<<<< HEAD
 	Logger      *log.ZapLogger
+=======
+	Logger      log.Logger
+>>>>>>> 989d433 (Create a separate package for proto logic)
 }
 
 type Operation struct {
 	topic            string
+<<<<<<< HEAD
 	schema           *srclient.Schema
 	rowToProtoSchema func(row []string) (*orgPb.Organization, error)
+=======
+	key              string
+	schema           *srclient.Schema
+	rowToProtoSchema func(row []string) *orgPb.Organization
+>>>>>>> 989d433 (Create a separate package for proto logic)
 }
 
 func (op Operation) GetNewKafkaWriter(config Config) *kafka.Writer {
@@ -52,7 +76,11 @@ func (op Operation) GetNewKafkaWriter(config Config) *kafka.Writer {
 	return w
 }
 
+<<<<<<< HEAD
 func (op Operation) IngestFilePROTO(config Config, fileTypeName string) error {
+=======
+func (op Operation) IngestFilePROTO(config Config, fileTypeName string) {
+>>>>>>> 989d433 (Create a separate package for proto logic)
 
 	switch fileTypeName {
 
@@ -65,6 +93,7 @@ func (op Operation) IngestFilePROTO(config Config, fileTypeName string) error {
 				break
 			}
 			if err != nil {
+<<<<<<< HEAD
 				config.Logger.Fatalf(config.Context, err.Error())
 				return err
 			}
@@ -81,13 +110,33 @@ func (op Operation) IngestFilePROTO(config Config, fileTypeName string) error {
 			if err != nil {
 				config.Logger.Fatalf(config.Context, fmt.Sprintf("error serializing message: %w", err))
 			}
+=======
+				log.Fatal(err)
+			}
+			// Serialise row using schema
+			orgSchema := op.rowToProtoSchema(row)
+			value, err := proto.Marshal(orgSchema)
+			valueBytes, _ := op.schema.Codec().BinaryFromNative(nil, value)
+
+			//Combine row bytes with schema id to make a record
+			var recordValue []byte
+			schemaIDBytes := GetSchemaIdBytes(op.schema)
+			recordValue = append(recordValue, byte(0))
+			recordValue = append(recordValue, schemaIDBytes...)
+			recordValue = append(recordValue, valueBytes...)
+>>>>>>> 989d433 (Create a separate package for proto logic)
 
 			// Put the row on the topic
 			err = w.WriteMessages(
 				config.Context,
 				kafka.Message{
+<<<<<<< HEAD
 					Key:   []byte(""),
 					Value: valueBytes,
+=======
+					Key:   []byte(op.key),
+					Value: recordValue,
+>>>>>>> 989d433 (Create a separate package for proto logic)
 				},
 			)
 			if err != nil {
@@ -95,5 +144,8 @@ func (op Operation) IngestFilePROTO(config Config, fileTypeName string) error {
 			}
 		}
 	}
+<<<<<<< HEAD
 	return nil
+=======
+>>>>>>> 989d433 (Create a separate package for proto logic)
 }
