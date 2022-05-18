1e55059373a138ca0701db00b8451d56d4cf0611
An Nguyen <annguyen@KLUKMAC412536.local>
2022-05-18 21:15:05 +0100

    remove status map

--- src/pkg/healthcheck/health_checker.go
+++ src/pkg/healthcheck/health_checker.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"sync"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
 	healthgrpc "google.golang.org/grpc/health/grpc_health_v1"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
@@ -13,12 +11,12 @@ import (
 type HealthServer struct {
 	mu sync.Mutex
 	// statusMap stores the serving status of the services this HealthServer monitors.
-	statusMap map[string]healthpb.HealthCheckResponse_ServingStatus
+	status healthpb.HealthCheckResponse_ServingStatus
 }
 
 func NewHealthServer() *HealthServer {
 	return &HealthServer{
-		statusMap: make(map[string]healthpb.HealthCheckResponse_ServingStatus),
+		status: healthpb.HealthCheckResponse_NOT_SERVING,
 	}
 }
 
@@ -31,12 +29,9 @@ func (s *HealthServer) Check(ctx context.Context, in *healthpb.HealthCheckReques
 			Status: healthpb.HealthCheckResponse_SERVING,
 		}, nil
 	}
-	if status, ok := s.statusMap[in.Service]; ok {
-		return &healthpb.HealthCheckResponse{
-			Status: status,
-		}, nil
-	}
-	return nil, grpc.Errorf(codes.NotFound, "Unknown Service")
+	return &healthpb.HealthCheckResponse{
+		Status: s.status,
+	}, nil
 }
 
 func (s *HealthServer) Watch(in *healthpb.HealthCheckRequest, stream healthgrpc.Health_WatchServer) error {
@@ -49,6 +44,6 @@ func (s *HealthServer) Watch(in *healthpb.HealthCheckRequest, stream healthgrpc.
 // or insert a new service entry into the statusMap.
 func (s *HealthServer) SetServingStatus(service string, status healthpb.HealthCheckResponse_ServingStatus) {
 	s.mu.Lock()
-	s.statusMap[service] = status
+	s.status = status
 	s.mu.Unlock()
 }
