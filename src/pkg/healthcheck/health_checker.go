9a8e9f1d0028d824dfca9db00577abffd59b9eea
An Nguyen <annguyen@KLUKMAC412536.local>
2022-05-23 10:41:05 +0100

    remove watch, request

--- src/pkg/healthcheck/health_checker.go
+++ src/pkg/healthcheck/health_checker.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"sync"
 
-	healthgrpc "google.golang.org/grpc/health/grpc_health_v1"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
@@ -19,24 +18,13 @@ func NewHealthServer() *HealthServer {
 	}
 }
 
-func (s *HealthServer) Check(ctx context.Context, in *healthpb.HealthCheckRequest) (*healthpb.HealthCheckResponse, error) {
+func (s *HealthServer) Check(ctx context.Context) (*healthpb.HealthCheckResponse, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	if in.Service == "" {
-		// check the server overall health status.
-		return &healthpb.HealthCheckResponse{
-			Status: healthpb.HealthCheckResponse_SERVING,
-		}, nil
-	}
+
 	return &healthpb.HealthCheckResponse{
 		Status: s.status,
 	}, nil
-}
-
-func (s *HealthServer) Watch(in *healthpb.HealthCheckRequest, stream healthgrpc.Health_WatchServer) error {
-	return stream.Send(&healthgrpc.HealthCheckResponse{
-		Status: healthgrpc.HealthCheckResponse_SERVING,
-	})
 }
 
 // SetServingStatus is called when need to reset the serving status of a service
